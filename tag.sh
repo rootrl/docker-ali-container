b73be4fd705f12e5982b4953c03f3c5bcbf4bcdc
rootrl <rootrl@github.com>
2021-09-14 14:40:35 +0800

    add tag.sh

---
+++ tag.sh
@@ -0,0 +1,5 @@
+#/usr/bin/env bash
+
+git tag -a release-cluster-proportional-autoscaler-amd64-v1.8.3 -m "cpa/cluster-proportional-autoscaler-amd64:1.8.3"
+git tag
+git push origin release-cluster-proportional-autoscaler-amd64-v1.8.3
